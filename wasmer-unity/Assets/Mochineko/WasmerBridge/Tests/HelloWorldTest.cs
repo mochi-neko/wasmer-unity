dcde19112b58da80fab773ce21f6e706bbcdb3d0
mochi-neko <t.o.e.4315@gmail.com>
2023-02-11 10:53:45 +0900

    Writing APIs as unit test without implementations

---
+++ wasmer-unity/Assets/Mochineko/WasmerBridge/Tests/HelloWorldTest.cs
@@ -0,0 +1,112 @@
+using System;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Mochineko.WasmerBridge.Tests
+{
+    internal sealed class HelloWorldTest
+    {
+        [Test, RequiresPlayMode(false)]
+        public void HelloWorld()
+        {
+            // WebAssembly Text Format
+            const string wat = @"
+(module
+  (type $t0 (func))
+  (import """" ""hello"" (func $.hello (type $t0)))
+  (func $run
+    call $.hello
+  )
+  (export ""run"" (func $run))
+)";
+
+            // Specify engine.
+            using var engine = new Engine();
+
+            // Create store.
+            using var store = new Store(engine);
+
+            // Compile wasm.
+            using var module = new Module(store, wat);
+
+            // Define "hello" function as import object.
+            var importObject = new ImportObject();
+            bool helloCalled = false;
+            importObject.AddFunction("Mochineko.WasmerBridge.Tests", "hello", () => helloCalled = true);
+
+            // Instantiate module.
+            var instance = module.Instantiate(store, module, importObject);
+
+            // Get exported function.
+            var run = instance.ExportFunction<Unit>(store, "run");
+
+            // Call exported function.
+            run.Invoke();
+
+            // Assert flag.
+            helloCalled.Should().Be(true);
+        }
+    }
+
+    public sealed class Engine : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+
+    public sealed class Store : IDisposable
+    {
+        public Store(Engine engine)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+
+    public sealed class Module : IDisposable
+    {
+        public Module(Store store, string wat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Instance Instantiate(Store store, Module module, ImportObject importObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class ImportObject
+    {
+        public void AddFunction<T>(string nameSpace, string functionName, Func<T> func)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class Instance
+    {
+        public Func<T> ExportFunction<T>(Store store, string run)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public sealed class Unit
+    {
+        private Unit()
+        {
+        }
+
+        public static readonly Unit Default = new();
+    }
+}
